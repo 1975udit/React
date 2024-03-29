625a9d5136f59cdfbafd04e24d18aee5e33d4b2e
1975udit <113414526+1975udit@users.noreply.github.com>
2024-03-29 22:42:56 +0000

    new

--- 01vite/src/App.jsx
+++ 01vite/src/App.jsx
@@ -1,8 +1,13 @@
-import chai from './chai'
+import Chai from './Chai.jsx'  // importing function from any file
 function App() {
   return (
-    <chai />
+    <>           
+    <Chai />
+    <h2>I am thw second Heading</h2>
+    </>
   )
+  // we can only return one element from return so we use this <></> whis is called "fregement"
 }
 
 export default App
+// way of exporting functions or methods
--- 01vite/src/Chai.jsx
+++ 01vite/src/Chai.jsx
@@ -1,6 +1,6 @@
-function chai(){
+function Chai(){
     return(
         <h2>I am from chai.js</h2>
     )
 }
-export default chai
+export default Chai
