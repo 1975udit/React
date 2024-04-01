e77615055e0eaf68481f043cfeff772d4669aee4
1975udit <113414526+1975udit@users.noreply.github.com>
2024-04-01 17:40:49 +0000

    new

---
+++ project_01/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import Card from './components/card'
+import Nav from './components/nav'
+import Crousel from './components/crousel'
+
+function App() {
+
+  return (
+    <>
+    <Nav />
+   <Card />
+    </>
+  )
+}
+
+export default App
